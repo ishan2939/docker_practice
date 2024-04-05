e51e79787d99ec6c9367752b7800503075f0b417
ishan2939 <ishan.h@simformsolutions.com>
2024-04-05 10:47:50 +0530

    fix: change the url to hardcore value

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -11,23 +11,23 @@ import getParameter from "../utils/getParameter"
 //     }
 // })();
 
-// const instance = axios.create({
-//     baseURL: url
-// })
+const instance = axios.create({
+    baseURL: "http://54.237.223.158:8000/api"
+})
 
-const instance = (async () => {
-    try {
-        const mongoURI = await getParameter('BACKEND_URL_ISHAN');
+// const instance = (async () => {
+//     try {
+//         const mongoURI = await getParameter('BACKEND_URL_ISHAN');
 
-        const axiosInstance = axios.create({
-            baseURL: mongoURI,
-        });
+//         const axiosInstance = axios.create({
+//             baseURL: mongoURI,
+//         });
 
-        return axiosInstance;
-    } catch (error) {
-        console.error('Error fetching parameter:', error);
-    }
-})();
+//         return axiosInstance;
+//     } catch (error) {
+//         console.error('Error fetching parameter:', error);
+//     }
+// })();
 
 
 export default instance
