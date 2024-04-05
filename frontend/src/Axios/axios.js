c67fc1c86d09d203e035d660837e48de735bc587
ishan2939 <ishan.h@simformsolutions.com>
2024-04-05 10:44:06 +0530

    fix: change the axios function

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,12 +1,33 @@
 import axios from "axios"
 import getParameter from "../utils/getParameter"
 
-let url;
-(async ()=>{
-     url = await getParameter('BACKEND_URL_ISHAN');
+// let url;
+// (async ()=>{
+//     try{
+//         url = await getParameter('BACKEND_URL_ISHAN');
+//     }
+//     catch (err){
+//         console.log("Error occured: ", err);
+//     }
+// })();
+
+// const instance = axios.create({
+//     baseURL: url
+// })
+
+const instance = (async () => {
+    try {
+        const mongoURI = await getParameter('BACKEND_URL_ISHAN');
+
+        const axiosInstance = axios.create({
+            baseURL: mongoURI,
+        });
+
+        return axiosInstance;
+    } catch (error) {
+        console.error('Error fetching parameter:', error);
+    }
 })();
 
-const instance = axios.create({
-    baseURL: url
-})
+
 export default instance
