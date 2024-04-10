530339ded58e3683fe7d8ed17546c107b0c97e23
ishan2939 <ishan.h@simformsolutions.com>
2024-04-10 15:03:11 +0530

    fix: changed axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,33 +1,14 @@
-import axios from "axios"
-
-
-// let url;
-// (async ()=>{
-//     try{
-//         url = await getParameter('BACKEND_URL_ISHAN');
-//     }
-//     catch (err){
-//         console.log("Error occured: ", err);
-//     }
-// })();
-
-const instance = axios.create({
-    baseURL: "http://44.193.84.212:8000/api"
-})
-
-// const instance = (async () => {
-//     try {
-//         const mongoURI = await getParameter('BACKEND_URL_ISHAN');
-
-//         const axiosInstance = axios.create({
-//             baseURL: mongoURI,
-//         });
-
-//         return axiosInstance;
-//     } catch (error) {
-//         console.error('Error fetching parameter:', error);
-//     }
-// })();
-
-
-export default instance
+import axios from "axios";
+import getParameter from "../utils/getParameter";
+let url;
+export default (async () => {
+    try {
+        url = await getParameter("BACKEND_URL_ISHAN");
+        const instance = axios.create({
+            baseURL: url,
+        });
+        return instance;
+    } catch (err) {
+        console.log("Error occured: ", err);
+    }
+})();
